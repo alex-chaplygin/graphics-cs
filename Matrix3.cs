70e8bfc8ea57d0e2f89c4e2e5180ec10c830e747
alex-chaplygin <alex_chaplygin@mail.ru>
2021-03-23 12:18:18 +0300

    Класс матрицы 3x3

---
+++ Matrix3.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicsLibrary
+{
+    /// <summary>
+    ///   Класс матрицы 3x3 для 2D графики
+    /// </summary>
+    public class Matrix3
+    {
+	/// <summary>
+	///   внутренний массив матрицы
+	/// </summary>
+        double[,] matrix;
+
+	/// <summary>
+	///   Создает новую единичную матрицу
+	/// </summary>
+        public Matrix3()
+        {
+            matrix = new double[3, 3];
+            Identity();
+        }
+
+	/// <summary>
+	///   Устанавливает единицы по главной диагонали, остальные значения - 0
+	/// </summary>
+        public void Identity()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (i==j)
+                    {
+                        matrix[i, j] = 1;
+                    }
+                    else
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+        }
+
+	/// <summary>
+	///   Создает матрицу переноса
+	/// </summary>
+        /// <param name="tx">величина перемещения по оси x</param>
+        /// <param name="ty">величина перемещения по оси y</param>
+        public void Translate(double tx, double ty)
+        {
+            matrix[0, 2] = tx;
+            matrix[1, 2] = ty;
+        }
+
+	/// <summary>
+	///   Создает матрицу масштабирования
+	/// </summary>
+        /// <param name="sx">коэффициент масштабирования по оси x</param>
+        /// <param name="sy">коэффициент масштабирования по оси y</param>
+        public void Scale(double sx, double sy)
+        {
+            matrix[0, 0] = sx;
+            matrix[1, 1] = sy;
+        }
+
+	/// <summary>
+	///   Создает матрицу поворота
+	/// </summary>
+        /// <param name="angle">угол поворота против часовой стрелки в радианах</param>
+        public void Rotate(double angle)
+        {
+            matrix[0, 0] = Math.Cos(angle);
+            matrix[0, 1] = -Math.Sin(angle);
+            matrix[1, 0] = Math.Sin(angle);
+            matrix[1, 1] = Math.Cos(angle);
+        }
+
+	/// <summary>
+	///   Умножение текущей матрицы на вектор
+	/// </summary>
+        /// <param name="v">вектор на который умножается матрица</param>
+        /// <returns>вектор, полученный в результате произведения</returns>
+        public Vector3 Mult(Vector3 v)
+        {
+            return new Vector3(
+                v.vect[0] * matrix[0, 0] + v.vect[1] * matrix[0, 1] + matrix[0, 2],
+                v.vect[0] * matrix[1, 0] + v.vect[1] * matrix[1, 1] + matrix[1, 2]);
+        }
+    }
+}
